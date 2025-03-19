8f527a21883336d09ae28d7d84e307c9cf6e986a
Leon Gabriel <g73346920@gmail.com>
2025-03-19 21:31:39 +0300

    Initial commits.

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { ProductDetails } from "@/components/product-details";
+import { stripe } from "@/lib/stripe";
+
+
+
+export default async function ProductPage({
+  params, }: {
+    params: { id: string };
+  }) {
+  const product = await stripe.products.retrieve(params.id, {
+    expand: ["default_price"]
+  });
+  return <ProductDetails product={product}/>
+}
